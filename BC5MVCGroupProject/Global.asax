183e8c9f0af274e07da44f140196c4cceabd50e4
gregory.carverprovalus <gregory.carverprovalus@gmail.com>
2018-04-09 10:00:01 -0500

    Add project files.

---
+++ BC5MVCGroupProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BC5MVCGroupProject.MvcApplication" Language="C#" %>
---
+++ BC5MVCGroupProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BC5MVCGroupProject.Startup))]
+namespace BC5MVCGroupProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
